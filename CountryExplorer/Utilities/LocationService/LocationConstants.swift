496f2cda85de9fd5b3d3405e2d76f6d10168bba3
AhmedFitoh <ahmed.fitoh.zakrya@protonmail.com>
2025-03-26 05:57:39 +0200

    Feature: - Add location service

---
+++ CountryExplorer/Utilities/LocationService/LocationConstants.swift
@@ -0,0 +1,13 @@
+//
+//  LocationConstants.swift
+//  CountryExplorer
+//
+//  Created by AhmedFitoh on 3/26/25.
+//
+
+import Foundation
+
+enum LocationConstants {
+    static let locationPermissionDenied = "LocationPermissionDenied"
+    static let defaultCountryCode = "EG"
+}
